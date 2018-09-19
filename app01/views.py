be924e9ffb92dec82b8d753e6886a1ca4aef618d
zyjwudi <473450296@qq.com>
2018-09-19 12:00:36 +0800

    session

--- app01/views.py
+++ app01/views.py
@@ -1,8 +1,9 @@
 # forms组件
 from django.shortcuts import render,HttpResponse,redirect
 from app01.models import login
-
-
+def add_login(request):
+    obj = login.objects.create(usename='cjj',password='123')
+    return HttpResponse('ok')
 def login_path(request):
     print(2)
     if request.method == 'POST':
@@ -27,3 +28,48 @@ def index(request):
         return render(request,'login_win.html',{'use':use})
     else:
         return redirect('/login/')
+
+def set_session(request):
+    if request.method == 'POST':
+        usename = request.POST.get('usename')
+        password = request.POST.get('password')
+        obj = login.objects.filter(usename=usename, password=password).first()
+        if obj:
+            request.session['is_login'] = True
+            request.session['usename']=usename
+            return HttpResponse('登录成功')
+    return render(request, 'login_test.html')
+
+def get_session(request):
+    get_data = request.session.get('is_login',default = None)
+    use = request.session.get('usename', default=None)
+    if get_data:
+        return render(request,'login_win.html',{'use':use})
+    else:
+        return redirect('/set_session/')
+    # try:
+    #     back = request.session.exists('is_login')
+    #     # if back:
+    #     print(back)
+    #     return HttpResponse('okk')
+    #
+    #     # get_data = request.session['is_login']
+    #     #
+    #     # use = request.session['usename']
+    # except:
+    #     print(1111)
+    #     redirect('/set_session/')
+    # if get_data:
+    #     print (1)
+        # return HttpResponse('WDQO')
+    # return render(request,'login_win.html')
+    # else:
+    #     redirect('/set_session/')
+
+def del_session(request):
+    del request.session['is_login']
+    del request.session["usename"]
+    return HttpResponse('ok')
+
+
+
